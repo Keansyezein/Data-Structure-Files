286f3df1a6c33142a8382b8bf59dc7e817c10c06
Keansyezein <148431554+Keansyezein@users.noreply.github.com>
2025-09-18 19:01:40 +0800

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,109 @@
+package node;
+
+
+
+import java.util.*;
+
+
+
+
+
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+    
+    public Node(int data){
+        this.data = data;
+    }
+}
+    class BinaryTree{
+        Node root;
+        
+        public void inorder(Node n, List<Integer> out){
+            if(n == null){
+                return;
+            }
+            else{
+                inorder(n.left, out);
+                out.add(n.data);
+                inorder(n.right, out);
+            }
+        }
+        
+        public void preorder(Node n, List<Integer> out){
+            if(n == null){
+                return;
+            }
+            else{
+                out.add(n.data);
+                preorder(n.left, out);
+                preorder(n.right, out);
+            }
+        }
+        public void postorder(Node n, List<Integer> out){
+            if(n == null){
+                return;
+            }
+            else{
+                preorder(n.left, out);
+                preorder(n.right, out);
+                out.add(n.data);
+            }
+        }
+        
+        
+        public List<Integer> LevelOrder(Node n){
+            List<Integer> out = new ArrayList();
+            
+            if (n == null) {
+                return out;
+            }
+                
+            Queue<Node> q = new ArrayDeque<>();
+            q.add(n);
+            
+            while(!q.isEmpty()){
+                Node cur = q.remove();
+                out.add(cur.data);
+                if(cur.left != null){
+                    q.add(cur.left);
+                }
+                if(cur.right != null){
+                    q.add(cur.right);
+                }
+            }
+            
+            return out;
+        }
+    }
+
+
+class Main{
+        
+    
+        public static void main(String[] args){
+            BinaryTree t = new BinaryTree();
+            t.root = new Node(10);
+            t.root.right = new Node(5);
+            t.root.left.left = new Node(2);
+            
+            List<Integer> in = new ArrayList();
+            List<Integer> pre = new ArrayList();
+            List<Integer> post = new ArrayList();
+            
+            t.inorder(t.root, in);
+            System.out.println("");
+            t.preorder(t.root, in);
+            System.out.println("");
+            t.postorder(t.root, in);
+            System.out.println("");
+            t.LevelOrder(t.root);
+        }
+}
+        
+    
+    
+    
+    
